08a9af8c1f1953c043092cc1a913d9e52a57544d
pf <paulatfortuna@gmail.com>
2015-11-15 13:09:05 +0000

    Finalmente dividido em classes

---
+++ bdi_evacuation/src/evacuation/agents/IntermediateBDI.java
@@ -0,0 +1,12 @@
+package evacuation.agents;
+
+import evacuation.utils.Position;
+import jadex.micro.annotation.Agent;
+
+/**
+ * Created by Paula on 15/11/2015.
+ */
+@Agent
+public class IntermediateBDI extends EvacuationAgentBDI {
+
+}

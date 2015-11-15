a1749ca6b798ef31349d23be76c2e279a1ef2780
pf <paulatfortuna@gmail.com>
2015-11-15 00:47:31 +0000

    Diferentes comportamentos previstos

---
+++ bdi_evacuation/src/evacuation/agents/HerdingBDI.java
@@ -0,0 +1,19 @@
+package evacuation.agents;
+
+import jadex.bdiv3.BDIAgent;
+import jadex.bdiv3.annotation.*;
+import jadex.bdiv3.runtime.ChangeEvent;
+import jadex.micro.annotation.Agent;
+import jadex.micro.annotation.Description;
+
+//@Agent
+//@Description("An agent with an updating belief.")
+public class HerdingBDI extends WandererBDI{
+
+    //@Agent
+    //protected BDIAgent agent;
+
+    void findExit(){
+
+    }
+}
---
+++ bdi_evacuation/src/evacuation/utils/TypesObjects.java
@@ -0,0 +1,12 @@
+package evacuation.utils;
+
+/**
+ * Created by Paula on 14/11/2015.
+ */
+public class TypesObjects {
+
+    public static String DOOR = "exit_door";
+    public static String INCIDENT = "incident";
+    public static String TERRAIN = "terrain";
+    public static String WANDERER = "wanderer";
+}

dbba2dbe9cd3c8d56de00490d8a2686e2dcb1e81
pf <paulatfortuna@gmail.com>
2015-11-18 14:36:50 +0000

    Mais reestruturação

---
+++ bdi_evacuation/src/evacuation/utils/WorldMethods.java
@@ -0,0 +1,14 @@
+package evacuation.utils;
+
+import jadex.extension.envsupport.environment.space2d.Grid2D;
+
+public class WorldMethods {
+
+    protected Grid2D space;
+
+    WorldMethods(Grid2D space){
+        this.space = space;
+    }
+
+
+}

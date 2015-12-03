cbb1e780920f30f02b8a0c3819c4d5a56628d329
jpmmaia <jpmmaia@gmail.com>
2015-12-03 23:04:46 +0000

    Adding test for FindPosition goal of Explorer.

--- bdi_evacuation/test/evacuation/utils/explorer/tests/ExplorerTester.java
+++ bdi_evacuation/test/evacuation/utils/explorer/tests/ExplorerTester.java
@@ -47,4 +47,36 @@ public class ExplorerTester
 		moveExplorer(explorer, 17);
 		Assert.assertEquals(new Position(0, 11), explorer.getPosition());
 	}
+
+	@Test
+	public void testFindPosition()
+	{
+		Explorer explorer = new Explorer(mTerrain, new Position(1, 1));
+
+		// Checkpoint 1:
+		Position checkpoint1 = new Position(4, 1);
+		explorer.setGoal(checkpoint1);
+		moveExplorer(explorer, 3);
+		Assert.assertEquals(checkpoint1, explorer.getPosition());
+		moveExplorer(explorer, 1);
+		Assert.assertEquals(checkpoint1, explorer.getPosition());
+
+		// Checkpoint 2:
+		Position checkpoint2 = new Position(10, 3);
+		explorer.setGoal(checkpoint2);
+		moveExplorer(explorer, 8);
+		Assert.assertEquals(checkpoint2, explorer.getPosition());
+
+		// Checkpoint 3:
+		Position checkpoint3 = new Position(18, 5);
+		explorer.setGoal(checkpoint3);
+		moveExplorer(explorer, 10);
+		Assert.assertEquals(checkpoint3, explorer.getPosition());
+
+		// Obstacle:
+		Position obstacle = new Position(16, 3);
+		explorer.setGoal(obstacle);
+		moveExplorer(explorer, 4);
+		Assert.assertEquals(new Position(16, 4), explorer.getPosition());
+	}
 }
